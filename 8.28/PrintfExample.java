a2b6ae342adce6a43a2b7a82c8707ff931e7723a
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-28 21:46:56 +0900

    Update PrintfExample.java

--- 8.28/PrintfExample.java
+++ 8.28/PrintfExample.java
@@ -1,1 +1,12 @@
+public class PrintfExample {
 
+	public static void main(String[] args) {
+		int value = 123;
+		System.out.printf("상품의 가격:%d\n", value);
+		System.out.printf("상품의 가격:%d\n", value);
+		System.out.printf("상품의 가격:%d\n", value);
+		
+
+	}
+
+}
