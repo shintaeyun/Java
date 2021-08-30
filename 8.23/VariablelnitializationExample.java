f6984e52791165cb3ce0838ea5903e95d2aa1e23
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-08-30 22:43:35 +0900

    Update VariablelnitializationExample.java

--- 8.23/VariablelnitializationExample.java
+++ 8.23/VariablelnitializationExample.java
@@ -3,6 +3,6 @@ public class VariableInitializationExample {
 	public static void main(String[] args) {
 		int value = 30;
 		int result = value + 10;
-		System.out.println(result);
+		System.out.println(result);  
 	}
 }
