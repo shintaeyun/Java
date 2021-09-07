ae2e30914ad1e59bd076f470dd954c7f4881360c
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-07 19:37:30 +0900

    Update and rename while 무한반복 -1일 적으면 멈춤.java to while 무한반복 -1 적으면 멈춤.java

---
+++ 9.07/while 무한반복 -1 적으면 멈춤.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Ex_04 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		while(true) 
+		{
+			int num = sc.nextInt();
+			sum += num;
+			if (num == -1) 
+			{
+				sum = sum - num; break;
+			}
+			
+		}
+		System.out.println(sum);
+
+	}
