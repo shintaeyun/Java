ce8983da8c0f271cced40ad8a058d9d70c5653dd
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-18 22:14:07 +0900

    Update 알파벳.java

--- 9.18/알파벳.java
+++ 9.18/알파벳.java
@@ -1,1 +1,16 @@
+public class Ex_05 {
+	
+	public static void alpha() {
+		char a='a';
+		do {
+			System.out.print(a+" ");
+			a=(char)(a+1);
+		}while (a<= 'z');
+	}
+
+	public static void main(String[] args) {
+		alpha();
+	}
+
+}
 
