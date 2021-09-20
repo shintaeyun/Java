9e635f33a4b43a15409049392ec223dd3fd16df4
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-20 23:02:48 +0900

    Update 5개 입력받으면 전부 다 더함.java

--- 9.20/5개 입력받으면 전부 다 더함.java
+++ 9.20/5개 입력받으면 전부 다 더함.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
+
+public class Ex_07 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int cnt = 0, sum = 0;
+		
+		while (cnt < 5) {
+			int num = sc.nextInt();
+			cnt += 1;
+			if (num < 0) {
+				continue;
+			} else {
+				sum += num;
+			}
+			
+		}
+		System.out.printf("%d", sum);
+
+	}
+}
 
