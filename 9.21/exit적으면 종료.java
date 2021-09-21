3d1a7fbde0e480d2fb4f32e07ee94cdb54edae3f
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-21 23:20:21 +0900

    Update exit적으면 종료.java

--- 9.21/exit적으면 종료.java
+++ 9.21/exit적으면 종료.java
@@ -1,1 +1,17 @@
+import java.util.Scanner;
+
+public class Ex_08 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		while(true) {
+			String str = sc.next();
+			if (str.equals("exit")) 
+			{
+				System.out.println("종료되었습니다."); break;
+			}
+		}
+	}
+}
 
