3797b60c2e943568991496bd0fdac017bb943682
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-22 19:07:48 +0900

    Update Variety Sum.java

--- 9.22/Variety Sum.java
+++ 9.22/Variety Sum.java
@@ -1,1 +1,48 @@
+public class Ex_09 {
+	
+	
+	public static int Sum() {
+		int result = 0;
+		for (int i = 1; i <=10; i++) 
+		{
+			result += i;
+		}
+		return(result);
+	}
+
+	public static int Sum(int n1,int n2) {
+		int result = 0;
+		for (int i = n1; i <= n2; i++)
+		{
+			result += i;
+			
+		}
+		return result;
+	}
+	
+	public static double Sum(double n1, double n2) {
+		double result = 0;
+		for (double i = n1; i <= n2; i++) {
+			result += i;
+		}
+		return result;
+	}
+	
+	public static double Sum(double n1, int n2){
+		double result = 0;
+		for (double i = n1; i <= n2; i += 0.5) 
+		{
+			result += i;
+		}
+		return result;
+	}
+	public static void main(String[] args) {
+		System.out.println(Sum());
+		int n1 = 10, n2 = 100;
+		double nd1 = 5.5, nd2 = 10.5;
+		System.out.println(Sum(n1, n2));
+		System.out.println(Sum(nd1, nd2));
+		System.out.println(Sum(nd1, n1));
+	}
+}
 
