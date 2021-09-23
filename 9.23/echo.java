58f625d24081c4c2c1a0755e9be81e1439858c49
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-23 19:42:24 +0900

    Update echo.java

--- 9.23/echo.java
+++ 9.23/echo.java
@@ -1,1 +1,13 @@
+public class Ex_10 {
+	
+	private static void echo(String string, int n) {
+		for (int i = 0; i < n; i++) {
+			System.out.println(string);
+		}
+		
+	}
 
+	public static void main(String[] args) {
+		echo("안녕", 3);
+	}
+}
