1a3cb7b443392e202c6888d32e1cc7d1e2a1e41c
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-24 23:18:17 +0900

    Update CircleDemo2.java

--- 9.24/CircleDemo2.java
+++ 9.24/CircleDemo2.java
@@ -1,1 +1,22 @@
+ class Circle {
+	double radius;
+	
+	double findArea() {
+		return 3.14 * radius * radius;
+	}
+	
+	void show(double x, double y) {
+		System.out.printf("반지름 = %.1f, 넓이 = %.1f\n", x, y);
+	}
+}
 
+public class CircleDemo2 {
+	public static void main(String[] args) {
+		Circle myCircle = new Circle1();
+		
+		myCircle.radius = 10.0;
+		
+		myCircle.show(myCircle.radius, myCircle.findArea());
+
+	}
+}
