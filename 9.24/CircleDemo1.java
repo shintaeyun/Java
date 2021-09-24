aa42b7e6d9788251c797d2f26ec723d492ada76f
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-24 23:17:14 +0900

    Update CircleDemo1.java

--- 9.24/CircleDemo1.java
+++ 9.24/CircleDemo1.java
@@ -1,1 +1,19 @@
-
+public class CircleDemo1 {
+	static double radius;
+	
+	static double findArea() {
+		return 3.14 * radius * radius;
+	}
+	
+	static void show(double x, double y) {
+		System.out.printf("반지름 = %.1f, 넓이 = %.1f\n", x, y);
+	}
+	
+	public static void main(String[] args) {
+		radius = 10.0;
+		
+		double area = findArea();
+		
+		show(radius, area);
+	}
+}
