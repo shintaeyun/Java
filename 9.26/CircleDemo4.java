27841888cd7a40c938ad1199463a9e618f9ee34b
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-09-26 17:09:22 +0900

    Update CircleDemo4.java

--- 9.26/CircleDemo4.java
+++ 9.26/CircleDemo4.java
@@ -1,1 +1,27 @@
+class Circle {
+	double radius;
+	String color;
+	
+	
+	public Circle(double r, String c) {
+		radius = r;
+		color = c;
+	}
+	
+	public Circle(double r) {
+		radius = r;
+		color = "파랑";
+	}
+	
+	public Circle(String c) {
+		radius = 10.0;
+		color = c;
+	}
+
+	public Circle() {
+		radius = 10.0;
+		color = "빨강";
+	}
+}
+
 
