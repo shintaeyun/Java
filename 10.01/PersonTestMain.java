2052f151697dfab2ec8d9abb0ebf568981d6defd
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-01 12:19:32 +0900

    Update PersonTestMain.java

--- 10.01/PersonTestMain.java
+++ 10.01/PersonTestMain.java
@@ -1,1 +1,19 @@
+class Person {
+	public int age;
+	public long height;
+	public float weight;
+}
 
+public class PersonTestMain {
+
+	public static void main(String[] args) {
+		Person brother = new Person();
+		brother.age=10;
+		brother.height=170;
+		brother.weight=67;
+		System.out.println("age : " + brother.age);
+		System.out.println("height : " + brother.height);
+		System.out.println("weight : " + brother.weight);
+	}
+
+}
