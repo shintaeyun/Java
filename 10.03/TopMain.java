d0ba67c6b64d0c455219e137a0034c904dd76a41
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-03 23:23:56 +0900

    Update TopMain.java

--- 10.03/TopMain.java
+++ 10.03/TopMain.java
@@ -1,1 +1,21 @@
+class Top {
+	public int a;
+	public int b;
+	public int sum(int x, int y) {
+		return x + y;
+	}
+}
+public class TopMain {
+	public static void main(String[] args) {
+		Top num = new Top();
+		num.a = 100;
+		num.b = 200;
+		int s = num.sum(3, 5);
+		System.out.println("a는 " + num.a);
+		System.out.println("b는 " + num.b);
+		System.out.println("sum는 " + num.sum(num.a, num.b));
+		System.out.println("s는 " + s);
+	}
+
+}
 
