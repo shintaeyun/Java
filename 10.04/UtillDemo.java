19f0d13af8bae079cce16449145c4adae4e16429
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-04 17:54:44 +0900

    Update UtillDemo.java

--- 10.04/UtillDemo.java
+++ 10.04/UtillDemo.java
@@ -1,1 +1,13 @@
+class Util {
+	static int fourTimes (int i) {
+		return i * 4;
+	}
+}
+
+public class UtillDemo {
+	public static void main(String[] args) {
+		System.out.println(Util.fourTimes(5));
+	}
+
+}
 
