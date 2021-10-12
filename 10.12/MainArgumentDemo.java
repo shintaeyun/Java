887e5ec1ba6614f77db274669c9f40c392d7c746
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-12 23:30:48 +0900

    Update MainArgumentDemo.java

--- 10.12/MainArgumentDemo.java
+++ 10.12/MainArgumentDemo.java
@@ -1,5 +1,4 @@
 public class MainArgumentDemo {
-
 	public static void main(String[] args) {
 		if (args.length == 2) {
 			int i = Integer.parseInt(args[1]);
@@ -8,11 +7,9 @@ public class MainArgumentDemo {
 			System.out.println("어이쿠!");
 	}
 	
-	
 	public static void nPrintln(String s, int n) {
 		for (int i = 0; i < n; i++)
 			System.out.println(s);
 	}
-
 }
 
