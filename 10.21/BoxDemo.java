510745762733686776a0e1d549933bc120834789
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-21 23:26:04 +0900

    Update BoxDemo.java

--- 10.21/BoxDemo.java
+++ 10.21/BoxDemo.java
@@ -1,1 +1,20 @@
+class Box {
+	public Box() {
+		System.out.println("오렌지");
+	}
+}
+
+class ColoredBox extends Box {
+	public ColoredBox( ) {
+		System.out.println("사과");
+	}
+}
+
+public class BoxDemo {
+	public static void main(String[] args) {
+		ColoredBox cb = new ColoredBox();
+
+	}
+
+}
 
