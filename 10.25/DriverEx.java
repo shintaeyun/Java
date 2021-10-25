e30fc9d7e5229b7c7d6a7ac8f189d139f8bcb9bf
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-25 22:15:19 +0900

    Update DriverEx.java

--- 10.25/DriverEx.java
+++ 10.25/DriverEx.java
@@ -1,1 +1,40 @@
+class Vehicle {
+	public void run() {
+		System.out.println("차량이 달립니다.");
+	}
+}
 
+class Bus extends Vehicle {
+	@Override
+	public void run() {
+		System.out.println("버스가 달립니다.");
+	}
+	
+}
+
+class Taxi extends Vehicle {
+	@Override
+	public void run() {
+		System.out.println("택시가 달립니다.");
+	}
+}
+
+class Driver {
+	public void drive(Vehicle vehicle) {
+		vehicle.run();
+	}
+}
+
+
+
+public class DriverEx {
+
+	public static void main(String[] args) {
+		Driver d = new Driver();
+		Taxi t = new Taxi();
+		Bus b = new Bus();
+		d.drive(t); //매서드 오버라이딩은 예외
+		d.drive(b);
+	}
+
+}
