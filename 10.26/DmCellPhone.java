e8d4680b17e4cf5d04a047dddbc6c11fc7604bea
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-26 23:25:52 +0900

    Update DmCellPhone.java

--- 10.26/DmCellPhone.java
+++ 10.26/DmCellPhone.java
@@ -1,1 +1,26 @@
+public class DmCellPhone extends CellPhone{
+	//필드
+	//생성자
+	public DmCellPhone(String model, String color, int channel) {
+//		this.model = model;
+//		this.color = color;
+		super(model, color);
+		this.channel = channel;
+
+	}
+	
+	//메서드
+	void turnOnDmb() {
+		System.out.println("채널 " + channel + "번 DMB방송 수신을 시작합니다.");
+	}
+	
+	void turnOffDmb() {
+		System.out.println("DMB방송 수신을 멈춥니다.");
+	}
+	
+	public void ChangeChannel(int channel) {
+		this.channel = channel;
+		System.out.println("채널 " + channel + "번으로 바꿉니다.");
+	}
+}
 
