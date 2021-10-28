d7d44f7607793a9f12e20be6f30d871692303d5f
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-28 23:23:42 +0900

    Update InheritanceDemo.java

--- 10.28/InheritanceDemo.java
+++ 10.28/InheritanceDemo.java
@@ -1,1 +1,19 @@
+public class InheritanceDemo {
+
+	public static void main(String[] args) {
+		Circle c1 = new Circle();
+		Ball c2 = new Ball("빨간색 ");
+		
+		System.out.println("원: ");
+//		c1.secret();
+		c1.findRadius();
+		c1.findArea();
+		
+		System.out.println("공: ");
+		
+		c2.findRadius();
+		c2.findArea();
+	}
+
+}
 
