9346a017866ec5b66f07c5c3b5da3c98ac74a3fa
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-10-30 23:22:58 +0900

    Update StudentTest.java

--- 10.30/StudentTest.java
+++ 10.30/StudentTest.java
@@ -1,1 +1,11 @@
-
+public class StudentTest {
+	public static void main(String[] args) {
+		Student st = new Student("홍길동", 051412, 1208);
+		System.out.println(st.name + ", " + st.ssn + ", " + st.hakbun);
+		st.fall();
+		
+		Student st2 = new Student("전우치", 123456, 1209);
+		System.out.println(st2.name + ", " + st2.ssn + ", " + st2.hakbun);
+			
+	}
+}
