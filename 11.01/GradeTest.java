f092b4066b5238cd5a7255e0efb6aaf667712c76
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-01 22:11:15 +0900

    Update GradeTest.java

--- 11.01/GradeTest.java
+++ 11.01/GradeTest.java
@@ -1,1 +1,36 @@
+import java.util.Scanner;
 
+class Grade {
+	int Math;
+	int Kor;
+	int Eng;
+	
+	public Grade(int Math, int Kor, int Eng) {
+		this.Math = Math;
+		this.Kor = Kor;
+		this.Eng = Eng;
+	}
+	
+	int sum() {
+		return Math + Kor + Eng;
+		
+	}
+	
+	double avg() {
+		return (double)sum() / 3;
+	}
+}
+
+public class GradeTest {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int Math = sc.nextInt();
+		int Kor = sc.nextInt();
+		int Eng = sc.nextInt();
+		
+		Grade g = new Grade(Math, Kor, Eng);
+		System.out.println("합계 : " + g.sum());
+		System.out.printf("평균 : %.1f", g.avg());
+		
+	}
+}
