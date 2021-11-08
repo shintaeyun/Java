6dbac331b2d66a327082204671364c1c3eea11bb
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-08 22:26:22 +0900

    Update UpcastDeom1.java

--- 11.08/UpcastDeom1.java
+++ 11.08/UpcastDeom1.java
@@ -1,1 +1,33 @@
+class Person {
+	String name = "사람";
+	
+	void whoami() {
+		System.out.println("나는 사람이다.");
+	}
+}
 
+class Stud extends Person {
+	int number = 7;
+	
+	void work() {
+		System.out.println("나는 공부한다");
+	}
+}
+
+
+public class UpcastDemo1 {
+	public static void main(String[] args) {
+		//Person p = new Stud();
+		
+		//Person p;
+		//p = new Stud();
+		
+		Person p;
+		Stud s = new Stud();
+		p = s;
+		
+		//p.number = 1; 안됨
+		//p.work(); 안됌 Stud로 생성 되었도 형변환을 Person으로 해서 Person에 있는 것만 사용가능
+		p.whoami();
+	}
+}
