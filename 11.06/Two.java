b298f319143cc21d59c1a2718b65d21fc0e7fcd1
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-08 22:25:08 +0900

    Create Two.java

---
+++ 11.06/Two.java
@@ -0,0 +1,10 @@
+public class Two {
+	void print() {
+		//객체 생성
+		One o = new One();
+		//System.out.println(secret);
+		System.out.println(o.roommate);
+		System.out.println(o.child);
+		System.out.println(o.anybody);
+	}
+}
