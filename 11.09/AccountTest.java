9af8891f65ae69e8da5b8e871ed38276346d4c65
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-09 22:57:44 +0900

    Update AccountTest.java

--- 11.09/AccountTest.java
+++ 11.09/AccountTest.java
@@ -1,1 +1,68 @@
+public class AccountTest {
 
+	public static void main(String[] args) {
+		Account a = new Account("123-45", "김철수", 10000);
+		Account b = new Account("567-89", "박영희", 10000);
+		
+		System.out.println(a.getAno() +" "+ a.getOwner() + " " + a.getBalance());
+		System.out.println(b.getAno() + " " + b.getOwner() + " "+ b.getBalance());
+		
+		a.tranfer(b, 3000);
+		
+		System.out.println(a.getAno() +" "+ a.getOwner() + " " + a.getBalance());
+		System.out.println(b.getAno() + " " + b.getOwner() + " "+ b.getBalance());
+	}
+}
+
+class Account {
+	private String ano;		//계좌번호
+	private String owner;	//계좌주
+	private int balance;	//잔액
+	
+	public Account (String ano, String owner, int balance) {
+		this.ano = ano;
+		this.owner = owner;
+		this.balance = balance;
+	}
+	
+	
+	public String getAno() {
+		return ano;
+	}
+
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+
+	public String getOwner() {
+		return owner;
+	}
+
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+
+	public int getBalance() {
+		return balance;
+	}
+
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+
+	void tranfer(Account target, int amount) {
+		if (balance < amount) {
+			System.out.println("잔액이 없습니다.");
+		}
+		else {
+			balance -= amount;
+			target.balance += amount;
+		}
+	}
+}
