681fb4d9eb1142955e583a5e52b18f2c03c2f671
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-11 23:36:47 +0900

    Update ColoredCircleTest.java

--- 11.11/ColoredCircleTest.java
+++ 11.11/ColoredCircleTest.java
@@ -1,1 +1,34 @@
+class Circle {
+	int radius;
+	
+	Circle(int radius) {
+		this.radius = radius;
+	}
+	void show() {
+		System.out.println("반지름이 " + radius + "인 원인다.");
+	}
+}
+
+class ColoredCircle extends Circle {
+	String color;
+	
+	ColoredCircle(int radius, String color) {
+		super(radius);
+		this.color = color;
+	}
+	@Override
+	void show() {
+		System.out.println("반지름이 " + radius + "인 " + color + " 원이다.");
+	}
+}
+
+public class ColoredCircleTest {
+	public static void main(String[] args) {
+		Circle c1 = new Circle(5);
+		ColoredCircle c2 = new ColoredCircle(10, "빨간색");
+		
+		c1.show();
+		c2.show();
+	}
+}
 
