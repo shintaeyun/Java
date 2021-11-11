fa80f4a19cf8c8e83b35e67e44bcbeae8ddb2e5c
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-11 23:38:50 +0900

    Update MinerTest.java

--- 11.11/MinerTest.java
+++ 11.11/MinerTest.java
@@ -1,1 +1,31 @@
+public class MinerTest {
+
+	public static void main(String[] args) {
+		Miner mi1 = new Miner("말론");
+		Miner mi2 = new Miner("글로리아");
+		mi1.mine(3);
+		mi2.mine(2);
+		mi1.print();
+		mi2.print();
+
+	}
+}
+
+class Miner {
+	String name;
+	int coin;
+
+	public Miner(String name) {
+		this.name = name;
+	}
+
+	void mine(int n) {
+		coin = n;
+	}
+
+	void print() {
+		System.out.println("name : " + name + ", coin : " + coin);
+	}
+
+}
 
