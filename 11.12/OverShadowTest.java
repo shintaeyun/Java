4f6303e653307d7caa6f58b6900c20097a8f41b9
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-12 23:43:14 +0900

    Update OverShadowTest.java

--- 11.12/OverShadowTest.java
+++ 11.12/OverShadowTest.java
@@ -1,1 +1,19 @@
+class A {
+  
+	A() {
+		System.out.println("인수 0의 생성자입니다.");
+	}
+  
+	A(int a) {
+		this();
+		System.out.println("인수 1의 생성자입니다.");
+	}
+}
 
+public class OvershadowTest {
+	public static void main(String[] args) {
+		A a1 = new A();
+		A a2 = new A();
+
+	}
+}
