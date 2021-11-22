d542c4676b5c69dee3a776791d065e41804b621b
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-22 22:39:48 +0900

    Update One2.java

--- 11.06/One2.java
+++ 11.06/One2.java
@@ -1,1 +1,11 @@
+import Chap_06.One;
 
+public class One2 extends One{
+	void print() {
+		//System.out.println(secret);
+		//System.out.println(roommate); //디폴드 값도 안됨
+		System.out.println(child);
+		System.out.println(anybody);
+	}
+	
+}
