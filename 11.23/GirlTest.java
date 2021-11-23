ec0b9cbf40074416edf2d1a93a3630a6e31c5e6c
shintaeyun <86235914+shintaeyun@users.noreply.github.com>
2021-11-23 20:37:48 +0900

    Update GirlTest.java

--- 11.23/GirlTest.java
+++ 11.23/GirlTest.java
@@ -1,1 +1,60 @@
+class Girl {
+//	private String name;
+	protected String name;
+	
+	Girl (String name) {
+		this.name = name;
+	}
+	
+//	Girl() {
+//		
+//	}
+	
+	void show() {
+		System.out.println(name + "는 자바 초보자이다.");
+	}
+
+}
+
+class GoodGirl extends Girl{
+	
+	GoodGirl (String name) {
+		super(name);
+	}
+	
+	void show() {
+		System.out.println(name + "는 자바를 잘 안다.");
+	}
+}
+
+class BestGirl extends GoodGirl{
+	
+	BestGirl (String name) {
+		super(name);
+	}
+	
+	void show() {
+		System.out.println(name + "는 자바를 무지하게 잘 안다.");
+	}
+}
+
+public class GirlTest {
+
+	public static void main(String[] args) {
+//		Girl g1 = new Girl();
+//		Girl g2 = new GoodGirl();
+//		GoodGirl gg = new BestGirl();
+//		
+//		g2.show(); 
+//		gg.show();
+		
+		Girl[] girls = {new Girl("갑순이"), new GoodGirl("콩쥐"), new BestGirl("황진이")};
+		
+		for (Girl g : girls) 
+			g.show();
+	}
+	
+	
+	
+}
 
